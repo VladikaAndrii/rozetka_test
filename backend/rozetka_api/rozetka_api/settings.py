6098016301e08d47e6f1e6d8f5c867bd67ab35e7
Aleksandr <94639744+ls500pymaster@users.noreply.github.com>
2023-05-17 15:50:59 +0300

    added custom settings for .env

--- backend/rozetka_api/rozetka_api/settings.py
+++ backend/rozetka_api/rozetka_api/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-import env as env
+from dotenv import load_dotenv
+import dj_database_url
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +80,9 @@ WSGI_APPLICATION = "rozetka_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': dj_database_url.config(default=env.str('DATABASE_URL')),
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL')),
 }
 
 
