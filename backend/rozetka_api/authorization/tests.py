f7fa81684043ef58abc524e1573d9886bef7d2bf
VladikaAndrii <andrii_vladika@ukr.net>
2023-05-17 20:08:06 +0300

    Changing Django structures.

--- backend/rozetka_api/authorization/tests.py
+++
@@ -1,3 +0,0 @@
-from django.test import TestCasereq
-
-# Create your tests here.
---
+++ backend/rozetka_api/users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+urlpatterns = [
+    path("registration" ""),
+    path('login', ""),
+    path("logout", ""),
+]
